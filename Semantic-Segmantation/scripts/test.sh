f37338e7fd09da16d3165faf275601fe6cd67ccb
sukzoon1234 <44229462+sukzoon1234@users.noreply.github.com>
2023-02-02 02:40:49 +0900

    Delete Semantic-Segmantation directory

--- Semantic-Segmantation/scripts/test.sh
+++
@@ -1,2 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=8 python ./train/train.py --config ./configs/MFD_ours.yaml --test_mode \
-#--is_continue --start_epo 300    
--- Semantic-Segmantation/scripts/train.sh
+++
@@ -1,2 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=16 python train/train.py --config ./configs/MFD_ours.yaml \
-#--is_continue --start_epo 340 
